8fca092e8fb65e6ee0cab1078dac6b4691d0793f
jbridg12utk <jbridg12@vols.utk.edu>
2021-07-09 21:36:24 -0400

    added the gamestate features to the main game class

--- WormHole/Game1.cs
+++ WormHole/Game1.cs
@@ -21,19 +21,31 @@ namespace WormHole
         public static GraphicsDeviceManager _graphics { private set; get; }
         private SpriteBatch _spriteBatch;
         public SpriteFont Font { get; set; }
-        public static Player P1 {get; set;}   // one player across the entire game so make it here for use everywhere
+        public static Player P1 {get; set; }   // one player across the entire game so make it here for use everywhere
+                                               //Used to cycle through the different screens - CLos
+        public enum GameState
+        {
+            Main,
+            Instructions,
+            Pause,
+            Game,
+            Gameover
+        }
+
         public enum Direction { Up, Right, Down, Left }
+
+        //What is currently getting shown on screen
+        public static GameState CurrentState { get; set; }
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            CurrentState = GameState.Main;
 
             //Sets Full Screen, more experimentation needed
             //_graphics.IsFullScreen = true;
-
-
         }
 
         protected override void Initialize()
@@ -58,7 +70,11 @@ namespace WormHole
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.F1))
                 Exit();
-            
+
+            if (CurrentState == GameState.Game)
+                this.IsMouseVisible = false;
+            if (CurrentState == GameState.Main)
+                this.IsMouseVisible = true;
 
             ScreenManager.Instance.Update(gameTime);
             EntityManager.Instance.Update(gameTime);
