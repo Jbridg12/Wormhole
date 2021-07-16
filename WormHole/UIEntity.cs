af9c8aad1f1f2302753b5623b6ebee5e49566533
jbridg12utk <jbridg12@vols.utk.edu>
2021-07-16 16:47:06 -0400

    began UI implementation

---
+++ WormHole/UIEntity.cs
@@ -0,0 +1,20 @@
+﻿// UIEntity.cs
+// Contributors: Josh
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+namespace WormHole
+{
+    class UIEntity : GameObject
+    {
+        public UIEntity(Rectangle position, Texture2D texture) : base(position, texture)
+        {
+
+        }
+    }
+}
