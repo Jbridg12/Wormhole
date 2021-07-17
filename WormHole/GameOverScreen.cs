d5a6612db6985a5629511dcffcee04ade62af456
dbg9180 <70482975+dbg9180@users.noreply.github.com>
2021-07-17 16:38:13 -0600

    start on game over screen

---
+++ WormHole/GameOverScreen.cs
@@ -0,0 +1,51 @@
+﻿// GameScreen.cs
+// Contributors: Josh Bridges
+//
+// A parent class for the different screen types
+// All of the screens will inherit from this class
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace WormHole
+{
+    public class GameOverScreen
+    {
+        protected ContentManager content;
+        public SpriteFont Font { get; set; }
+        public Texture2D Display { get; set; }
+        public Dictionary<string, Texture2D> Displays { get; set; }
+
+        public List<Entity> Entities { get; set; }
+
+        public GameOverScreen(Texture2D texture, SpriteFont font)
+        {
+            this.Entities = new List<Entity>();
+            this.Font = font;
+            this.Display = texture;
+            this.Displays = null;
+        }
+        public GameOverScreen(Dictionary<string, Texture2D> textures, SpriteFont font)
+        {
+            this.Entities = new List<Entity>();
+            this.Font = font;
+            this.Displays = textures;
+            this.Display = null;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            Globals.ImageCenter(spriteBatch, Game1._graphics, this.Display);
+        }
+    }
+}
